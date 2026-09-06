0fda0fd87b59318d49403f829132b298365ab6fd
umarbashir-dotcom <umarbasheer449@gmail.com>
2026-09-06 15:23:07 +0500

    payment order fixed

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -86,8 +86,8 @@ const create_checkout_session = async (req, res) => {
             payment_method_types: ["card"],
             line_items,
             mode: "payment",
-            success_url: `https://kiryana-store.onrender.com/order/checkout-success/${order._id}`,
-            cancel_url: `https://kiryana-store.onrender.com/order/checkout-cancel/${order._id}`,
+            success_url: `https://kiryana-store-jet.vercel.app/order/checkout-success/${order._id}`,
+            cancel_url: `https://kiryana-store-jet.vercel.app/order/checkout-cancel/${order._id}`,
             metadata: { 
                 orderId: order._id.toString()
             }
